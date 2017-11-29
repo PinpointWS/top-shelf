82dad80139b32e7236efeac539c29870d783bd78
family <family@DESKTOP-51HS017.attlocal.net>
2017-11-28 19:19:08 -0500

    Added some styling to the band page.

--- top-shelf/src/app/core/core.module.ts
+++ top-shelf/src/app/core/core.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { NavComponent } from "./nav/nav.component";
 import { FooterComponent } from "./footer/footer.component";
 import { AppRoutingModule } from "../app-routing/app-routing.module";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 @NgModule({
   imports: [
     CommonModule,
     AppRoutingModule,
+    NgbModule
   ],
   exports: [        
     NavComponent,
